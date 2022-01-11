b42fce80733f11425bbfd9b7d201bf54222387f4
Nelson Roberts <nroberts@sbmcorp.com>
2022-01-10 23:36:48 -0800

    added voters route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
 
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
